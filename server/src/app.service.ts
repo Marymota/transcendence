1fb9bd89b2d131b741f958ba8831c93bb1a6c148
mar <mar@debian.debian.virtualbox.org>
2023-09-17 18:15:34 +0100

    Navbar logo add

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,11 @@
+
+/* Welcome message in the Home screen set up as a NestJS service using the '@Injectable' decorator */
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getWelcomeMsg() {
-    return { msg: 'Start building your Postgres-Nest-React Application' };
+    return { msg: 'PONG' };
   }
 }
